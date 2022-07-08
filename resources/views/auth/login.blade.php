6a7d75f3baf972986e67557c131060cdbf45071b
kenya.kitagawa <lick.to.rock@gmail.com>
2022-07-08 12:53:12 +0000

    user login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,11 +2,11 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>バンドメンバー募集掲示板</title>
+        <title>ログイン</title>
         <div class="card border-info mx-auto mt-4" style="width:fit-content">
             <div class="card-header">
                 <div class="text-center">
-                     <h2>バンドメンバー募集掲示板へようこそ！</h2>
+                     <h2>ログイン</h2>
                      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">                
                 </div>
             </div>
@@ -14,9 +14,23 @@
     </head>
     <body>
         <div class="row">
-            <div class="col-4 offset-4 mt-5">
-                {!! link_to_route('signup.get','新規登録はこちらから',[],['class'=>'btn btn-primary btn-lg btn-block']) !!}
-                {!! link_to_route('login','登録済の方はこちらから（ログイン）',[],['class'=>'btn btn-link btn-lg btn-block']) !!}
+            <div class="col-6 offset-3">
+                {!! Form::open(['route'=>'login.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('email','メールアドレス') !!}
+                    {!! Form::email('email',null,['class'=>'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password','パスワード') !!}
+                    {!! Form::password('password',['class'=>'form-control']) !!}
+                </div>
+            </div>
+            <div class="col-4 offset-4">
+                {!! Form::submit('ログイン', ['class'=>'btn btn-warning btn-block']) !!}
+                {!! Form::close() !!}
+                
+                <a href="/" class="btn btn-info btn-block mt-5">戻る</a>
             </div>
         </div>
         
@@ -26,6 +40,3 @@
         <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
     </body>
 </html>
-
-    
-    
